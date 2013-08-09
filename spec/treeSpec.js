2e4e7a02985b1e3097f13a5a9fd8c4e93c4290f5
Hack Reactor Students <github@hackreactor.com>
2013-08-09 16:46:15 -0700

    refactored addChild and contains method, passing all tests

--- spec/treeSpec.js
+++ spec/treeSpec.js
@@ -11,5 +11,19 @@ describe("tree", function() {
     expect('value' in tree).toBe(true);
   });
 
-  // Add more tests here to test the functionality of tree.
+  it("should be able to create a root node", function(){
+    var boss = tree.addChild(0);
+    expect(tree.contains(0)).toBe(true);
+  });
+
+  it("should be able to find a particular value in the tree", function(){
+    var boss = tree.addChild('Steve');
+    var CTO = boss.addChild('Harvey');
+    var CEO = boss.addChild('Frank');
+    var manager = CTO.addChild('Jenny');
+    expect(tree.contains('Steve')).toBe(true);
+    expect(tree.contains('Harvey')).toBe(true);
+    expect(tree.contains('Jenny')).toBe(true);
+    expect(tree.contains('Victor')).toBe(false);
+  });
 });
--- src/tree.js
+++ src/tree.js
@@ -1,15 +1,31 @@
-var makeTree = function(){
+var makeTree = function(value){
   var newTree = {};
-  newTree.value = undefined;
-  newTree.children = undefined;
+  newTree.value = value;
+  newTree.children = [];
+
+  _.extend(newTree,treeMethods);
 
   return newTree;
 };
 
 var treeMethods = {};
 
-treeMethods.addChild = function(){
+treeMethods.addChild = function(value){
+  var child = makeTree(value);
+  this.children.push(child);
+  return child;
 };
 
-treeMethods.contains = function(){
+treeMethods.contains = function(value){
+  if(this.value === value){
+    return true;
+  }
+  if (this.children.length) {
+    for (var i = 0; i < this.children.length; i++) {
+      if(this.children[i].contains(value)){
+        return true;
+      }
+    }
+  }
+  return false
 };
