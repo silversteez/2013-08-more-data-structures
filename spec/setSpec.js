e62882e37232f27a5350b06c4852b51aa7de7134
Hack Reactor Students <github@hackreactor.com>
2013-08-09 17:22:34 -0700

    modified methods for makeSet prototype, passing all tests

--- spec/setSpec.js
+++ spec/setSpec.js
@@ -10,4 +10,26 @@ describe("set", function() {
     expect(set.contains).toEqual(jasmine.any(Function));
     expect(set.remove).toEqual(jasmine.any(Function));
   });
+
+  it("should be able to add a value and check if the set contains that value", function(){
+    set.add('cake');
+    expect(set.contains('cake')).toEqual(true);
+  });
+
+  it("should be able to remove a value and see that the value is no longer in the set", function(){
+    set.add('cake');
+    set.remove('cake');
+    expect(set.contains('cake')).toEqual(false);
+  })
+
+  it("should add three values and remove two of them successfully", function(){
+    set.add('cake');
+    set.add('pie');
+    set.add('cobbler');
+    set.remove('pie');
+    set.remove('cobbler');
+    expect(set.contains('pie')).toEqual(false);
+    expect(set.contains('cobbler')).toEqual(false);
+    expect(set.contains('cake')).toEqual(true);
+  })
 });
--- src/set.js
+++ src/set.js
@@ -1,16 +1,28 @@
 var makeSet = function(){
   var set = Object.create(setPrototype); // fix me
-  set._storage = undefined;
+  set._storage = {};
   return set;
 };
 
 var setPrototype = {};
 
-setPrototype.add = function(){
+setPrototype.add = function(value){
+  this._storage[value] = true;
 };
 
-setPrototype.contains = function(){
+setPrototype.contains = function(value){
+  for(var key in this._storage){
+    if(key === value){
+      return true;
+    };
+  };
+  return false;
 };
 
-setPrototype.remove = function(){
+setPrototype.remove = function(value){
+  for(var key in this._storage){
+    if(key === value){
+      delete this._storage[value];
+    };
+  };
 };
